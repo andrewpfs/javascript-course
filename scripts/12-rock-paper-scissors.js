0284f8cfb45a97ff4f8b17a57a6fad912461bfc3
andrewpfs <152920778+andrewpfs@users.noreply.github.com>
2025-02-26 22:56:40 -0600

    adds arrow function and eventListener to game and got rid of onclick

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -9,10 +9,13 @@ let score = JSON.parse(localStorage.getItem('score')) ||
 
 let isAutoPlaying = false;
 let intervalId;
+//const autoPlay() => {
+
+//};
 function autoPlay(){
 
   if(!isAutoPlaying){
-     intervalId = setInterval(function(){
+     intervalId = setInterval(() =>{
       const playerMove = pickComputerMove();
       playGame(playerMove);
     }, 1000);
@@ -23,6 +26,20 @@ function autoPlay(){
     isAutoPlaying = false;
   }
 }
+document.querySelector('.js-rock-button')
+.addEventListener('click',() => {
+  playGame('rock');
+});
+
+document.querySelector('.js-paper-button')
+.addEventListener('click',() => {
+  playGame('paper');
+});
+
+document.querySelector('.js-scissors-button')
+.addEventListener('click',() => {
+  playGame('scissors');
+});
 
 function playGame(playerMove) {
  const computerMove = pickComputerMove();
